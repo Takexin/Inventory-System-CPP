b7b2207413f7176e49794ebb0412d9477c0d8573
Felipe Takeshi <ftrangel07@gmail.com>
2025-02-09 19:39:35 -0300

    add clangd lsp support

--- include/apiDB.hpp
+++ include/apiDB.hpp
@@ -1,50 +1,44 @@
-#include "sqlite3.h"
 #include "item.hpp"
-#include <iostream>
+#include "sqlite3.h"
 #include <string>
 
-
-void terminatePrepared(sqlite3_stmt *pStmt){
+void terminatePrepared(sqlite3_stmt *pStmt) {
   int verifier = sqlite3_finalize(pStmt);
-  if(verifier != 0){
+  if (verifier != 0) {
     std::cout << "Error in finalizing prepared statement \n";
-  }
-  else {
+  } else {
     std::cout << "finalized prepared statement sucessfully \n";
   }
 }
 
-
-
-sqlite3_stmt *prepareItemObject(sqlite3 *DB, std::string &statement){
-  //handle object prepare statement 
+sqlite3_stmt *prepareItemObject(sqlite3 *DB, std::string &statement) {
+  // handle object prepare statement
   int nByte = -1;
   sqlite3_stmt *stHandle = nullptr;
   const char *pzTail = nullptr;
-  int prepareStatement = sqlite3_prepare_v2(DB, statement.c_str(), nByte, &stHandle, &pzTail);
+  int prepareStatement =
+      sqlite3_prepare_v2(DB, statement.c_str(), nByte, &stHandle, &pzTail);
   std::cout << prepareStatement << '\n';
-  if (prepareStatement != 0){
+  if (prepareStatement != 0) {
     std::cout << "prepared statement failed. \n";
     return nullptr;
-  }
-  else{
-    std:: cout << "prepared statement was sucessfull. \n";
+  } else {
+    std::cout << "prepared statement was sucessfull. \n";
   }
   return stHandle;
 }
-bool stepItemObject(sqlite3_stmt *stHandle){
+bool stepItemObject(sqlite3_stmt *stHandle) {
   int result = sqlite3_step(stHandle);
-  if (result != 101 && result != 100){
-    std::cout << "step failed. \n" ;
+  if (result != 101 && result != 100) {
+    std::cout << "step failed. \n";
     return false;
-  }
-  else {
+  } else {
     std::cout << "step was sucessfull \n";
   }
   return true;
 }
 
-void createTable(sqlite3 *DB, std::string desiredStatement){
+void createTable(sqlite3 *DB, std::string desiredStatement) {
   sqlite3_stmt *preparedObject = prepareItemObject(DB, desiredStatement);
   if (preparedObject != nullptr) {
     bool stepTry = stepItemObject(preparedObject);
@@ -53,19 +47,18 @@ void createTable(sqlite3 *DB, std::string desiredStatement){
     }
   }
 }
-int idResultQuery(sqlite3_stmt *stHandle, int iCol){
+int idResultQuery(sqlite3_stmt *stHandle, int iCol) {
   int verifier = sqlite3_column_int(stHandle, iCol);
-  if(!verifier ){
+  if (!verifier) {
     std::cout << "Search unsucessfull, try again\n";
-  }
-  else{
+  } else {
     return verifier;
   }
   return -1;
 }
-int searchItem(sqlite3 *DB, std::string objectName){
+int searchItem(sqlite3 *DB, std::string objectName) {
   int searchResult = -1;
-  std::string statement = "SELECT id FROM items WHERE name = '" ;
+  std::string statement = "SELECT id FROM items WHERE name = '";
   statement.append(objectName);
   statement.append("'");
   sqlite3_stmt *preparedObject = prepareItemObject(DB, statement);
@@ -73,23 +66,22 @@ int searchItem(sqlite3 *DB, std::string objectName){
     bool stepTry = stepItemObject(preparedObject);
     if (stepTry != false) {
       searchResult = idResultQuery(preparedObject, 0);
-      if(searchResult != -1){
+      if (searchResult != -1) {
         terminatePrepared(preparedObject);
-      }
-      else{
+      } else {
         std::cout << sqlite3_errmsg(DB) << std::endl;
       }
 
-    }
-    else{
-      std::cout << sqlite3_errmsg(DB) << '\n';//don't mind the jank
+    } else {
+      std::cout << sqlite3_errmsg(DB) << '\n'; // don't mind the jank
     }
   }
   return searchResult;
 }
-void insertItemObject(sqlite3 *DB,item desiredItem){
-  //assuming table rows (name, quantity, price, category)
-  std::string statement = "INSERT INTO items(name, quantity, price, category_id) VALUES('";
+void insertItemObject(sqlite3 *DB, item desiredItem) {
+  // assuming table rows (name, quantity, price, category)
+  std::string statement =
+      "INSERT INTO items(name, quantity, price, category_id) VALUES('";
   statement.append(desiredItem.getName());
   statement.append("',");
   statement.append(std::to_string(desiredItem.getQuantity()));
@@ -98,10 +90,10 @@ void insertItemObject(sqlite3 *DB,item desiredItem){
   statement.append(",'");
   statement.append(std::to_string(searchItem(DB, desiredItem.getCategory())));
   statement.append("')");
-  std::cout << statement << '\n'; 
+  std::cout << statement << '\n';
 
-    //TODO
-    //EXECUTE FUNCTIONS ONLY IF PREVIOUS ROUTINE DID NOT FAIL
+  // TODO
+  // EXECUTE FUNCTIONS ONLY IF PREVIOUS ROUTINE DID NOT FAIL
   sqlite3_stmt *preparedObject = prepareItemObject(DB, statement);
   if (preparedObject != nullptr) {
     bool stepTry = stepItemObject(preparedObject);
@@ -109,17 +101,16 @@ void insertItemObject(sqlite3 *DB,item desiredItem){
       terminatePrepared(preparedObject);
     }
   }
-
 }
-void insertCat(sqlite3 *DB, std::string desiredCategory){
-  //assuming name only
+void insertCat(sqlite3 *DB, std::string desiredCategory) {
+  // assuming name only
   std::string statement = "INSERT INTO categories(name) VALUES('";
   statement.append(desiredCategory);
   statement.append("')");
-  std::cout << statement << '\n'; 
+  std::cout << statement << '\n';
 
-    //TODO
-    //EXECUTE FUNCTIONS ONLY IF PREVIOUS ROUTINE DID NOT FAIL
+  // TODO
+  // EXECUTE FUNCTIONS ONLY IF PREVIOUS ROUTINE DID NOT FAIL
   sqlite3_stmt *preparedObject = prepareItemObject(DB, statement);
   if (preparedObject != nullptr) {
     bool stepTry = stepItemObject(preparedObject);
@@ -127,14 +118,13 @@ void insertCat(sqlite3 *DB, std::string desiredCategory){
       terminatePrepared(preparedObject);
     }
   }
-
 }
 
-int deleteItem(sqlite3 *DB, std::string objectName){
+int deleteItem(sqlite3 *DB, std::string objectName) {
   int searchResult = -1;
-  std::string statement = "DELETE FROM items WHERE id = " ;
+  std::string statement = "DELETE FROM items WHERE id = ";
   int searchItemResult = searchItem(DB, objectName);
-  if(searchItemResult != -1){
+  if (searchItemResult != -1) {
     statement.append(std::to_string(searchItemResult));
     sqlite3_stmt *preparedObject = prepareItemObject(DB, statement);
     if (preparedObject != nullptr) {
@@ -142,35 +132,38 @@ int deleteItem(sqlite3 *DB, std::string objectName){
       if (stepTry != false) {
         terminatePrepared(preparedObject);
         return 0;
-      }
-      else{
-        std::cout << sqlite3_errmsg(DB) << '\n';//don't mind the jank
+      } else {
+        std::cout << sqlite3_errmsg(DB) << '\n'; // don't mind the jank
       }
     }
   }
   return searchResult;
 }
-int multiDeleteItem(sqlite3 *DB, std::string objectName){
-  //prepare statement binding (I'm cool now)
+int multiDeleteItem(sqlite3 *DB, std::string objectName) {
+  // prepare statement binding (I'm cool now)
   std::string statement = "DELETE FROM items WHERE id = ?1";
   sqlite3_stmt *preparedObject = prepareItemObject(DB, statement);
   if (preparedObject != nullptr) {
     int stepCount = 0;
     bool stepHandle = true;
-    int bindHandle = sqlite3_bind_text(preparedObject, 1, objectName.c_str(), -1, nullptr);
-    if(bindHandle == 100){
-      while(stepHandle == true ){stepHandle = stepItemObject(preparedObject); stepCount++;}
-      return stepCount-1;
+    int bindHandle =
+        sqlite3_bind_text(preparedObject, 1, objectName.c_str(), -1, nullptr);
+    if (bindHandle == 100) {
+      while (stepHandle == true) {
+        stepHandle = stepItemObject(preparedObject);
+        stepCount++;
+      }
+      return stepCount - 1;
+    } else {
+      std::cout << sqlite3_errmsg(DB) << '\n';
     }
-    else{std::cout << sqlite3_errmsg(DB) << '\n';}
   }
   return -1;
 }
 
-
-
-void editItem(sqlite3 *DB, std::string tableName, std::string objectName, std::string desiredColumn ,std::string desiredValue){
-  //table, object, column, value
+void editItem(sqlite3 *DB, std::string tableName, std::string objectName,
+              std::string desiredColumn, std::string desiredValue) {
+  // table, object, column, value
   int searchResult = -1;
   std::string statement = "UPDATE ";
   statement.append(tableName);
@@ -185,14 +178,14 @@ void editItem(sqlite3 *DB, std::string tableName, std::string objectName, std::s
     bool stepTry = stepItemObject(preparedObject);
     if (stepTry != false) {
       terminatePrepared(preparedObject);
-    }
-    else{
-      std::cout << sqlite3_errmsg(DB) << '\n';//don't mind the jank
+    } else {
+      std::cout << sqlite3_errmsg(DB) << '\n'; // don't mind the jank
     }
   }
 }
-void editItem(sqlite3 *DB, std::string tableName, int objectID, std::string desiredColumn ,std::string desiredValue){
-  //table, object, column, value
+void editItem(sqlite3 *DB, std::string tableName, int objectID,
+              std::string desiredColumn, std::string desiredValue) {
+  // table, object, column, value
   int searchResult = -1;
   std::string statement = "UPDATE ";
   statement.append(tableName);
@@ -208,9 +201,8 @@ void editItem(sqlite3 *DB, std::string tableName, int objectID, std::string desi
     bool stepTry = stepItemObject(preparedObject);
     if (stepTry != false) {
       terminatePrepared(preparedObject);
-    }
-    else{
-      std::cout << sqlite3_errmsg(DB) << '\n';//don't mind the jank
+    } else {
+      std::cout << sqlite3_errmsg(DB) << '\n'; // don't mind the jank
     }
   }
 }
--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,7 @@
-#include <iostream>
-#include <limits>
 #include "apiDB.hpp"
+#include <limits>
 
-//function prototypes
+// function prototypes
 void addItem(sqlite3 *DB);
 void addCat(sqlite3 *DB);
 void removeItem(sqlite3 *DB);
@@ -11,8 +10,7 @@ void searchTable(sqlite3 *DB);
 int main(int argc, char *argv[]) {
   sqlite3 *DB;
   int exit = 0;
-  exit =
-      sqlite3_open("inventory.db", &DB);
+  exit = sqlite3_open("inventory.db", &DB);
 
   if (exit) {
     std::cerr << "Error opening DB " << sqlite3_errmsg(DB) << std::endl;
@@ -20,16 +18,21 @@ int main(int argc, char *argv[]) {
   } else {
     std::cout << "Opened DB sucessfully\n";
   }
-  //usr, add itm, add cat, rmv itm, seach items, exit
+  // usr, add itm, add cat, rmv itm, seach items, exit
   const int options[] = {0, 1, 2, 3, 4};
   std::string startText =
-    " ___                      _                     ____            _\n"
-    "|_ _|_ ____   _____ _ __ | |_ ___  _ __ _   _  / ___| _   _ ___| |_ ___ _ __ ___\n"
-    " | || '_ \\ \\ / / _ \\ '_ \\| __/ _ \\| '__| | | | \\___ \\| | | / __| __/ _ \\ '_ ` _ \\\n"
-    " | || | | \\ V /  __/ | | | || (_) | |  | |_| |  ___) | |_| \\__ \\ ||  __/ | | | | |\n"
-    "|___|_| |_|\\_/ \\___|_| |_|\\__\\___/|_|   \\__, | |____/ \\__, |___/\\__\\___|_| |_| |_|\n"
-    "                                         |___/         |___/\n"
-    "-----------------------------------------------------------------------------------------------\n";
+      " ___                      _                     ____            _\n"
+      "|_ _|_ ____   _____ _ __ | |_ ___  _ __ _   _  / ___| _   _ ___| |_ ___ "
+      "_ __ ___\n"
+      " | || '_ \\ \\ / / _ \\ '_ \\| __/ _ \\| '__| | | | \\___ \\| | | / __| "
+      "__/ _ \\ '_ ` _ \\\n"
+      " | || | | \\ V /  __/ | | | || (_) | |  | |_| |  ___) | |_| \\__ \\ ||  "
+      "__/ | | | | |\n"
+      "|___|_| |_|\\_/ \\___|_| |_|\\__\\___/|_|   \\__, | |____/ \\__, "
+      "|___/\\__\\___|_| |_| |_|\n"
+      "                                         |___/         |___/\n"
+      "------------------------------------------------------------------------"
+      "-----------------------\n";
   std::cout << startText << '\n';
   int option = 0;
   const int exitOpt = 6;
@@ -38,42 +41,42 @@ int main(int argc, char *argv[]) {
   const int rmvOpt = 3;
   const int searchItemOpt = 4;
   const int searchCatOpt = 5;
-  const int searchTableOpt = 7; 
-  while(option != exitOpt){
-  std::cout << "\nSelect an option:" << "\n" << itemOpt << "-Add item\n" 
-    << catOpt << "-Add category\n" 
-    << rmvOpt << "-Remove item\n" 
-    << exitOpt << "-exit\n"; 
-  std::cin >> option;
-  std::cin.clear();
-  std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-  switch(option){
-    case searchTableOpt:{
+  const int searchTableOpt = 7;
+  while (option != exitOpt) {
+    std::cout << "\nSelect an option:" << "\n"
+              << itemOpt << "-Add item\n"
+              << catOpt << "-Add category\n"
+              << rmvOpt << "-Remove item\n"
+              << exitOpt << "-exit\n";
+    std::cin >> option;
+    std::cin.clear();
+    std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+    switch (option) {
+    case searchTableOpt: {
       searchTable(DB);
       break;
     }
-    case itemOpt:{
+    case itemOpt: {
       addItem(DB);
       break;
     }
-    case catOpt:{
+    case catOpt: {
       addCat(DB);
       break;
     }
-    case rmvOpt:{
+    case rmvOpt: {
       removeItem(DB);
       break;
     }
-    case exitOpt:{
+    case exitOpt: {
       break;
     }
-  }
-
+    }
   }
   sqlite3_close(DB);
   return 0;
 }
-void addItem(sqlite3 *DB){
+void addItem(sqlite3 *DB) {
   std::string desiredName;
   int desiredQuantity = 0;
   float desiredPrice = 0.0;
@@ -82,7 +85,7 @@ void addItem(sqlite3 *DB){
   std::cin >> desiredName;
   std::cin.clear();
   std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-  std::cout <<"\n\nPlease provide the quantity of the item: ";
+  std::cout << "\n\nPlease provide the quantity of the item: ";
   std::cin >> desiredQuantity;
   std::cin.clear();
   std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
@@ -97,7 +100,7 @@ void addItem(sqlite3 *DB){
   item desiredItem(desiredName, desiredQuantity, desiredPrice, desiredCat);
   insertItemObject(DB, desiredItem);
 }
-void addCat(sqlite3 *DB){
+void addCat(sqlite3 *DB) {
   std::string desiredName;
   std::cout << "\n\nPlease provide the name of the category: ";
   std::cin >> desiredName;
@@ -105,16 +108,15 @@ void addCat(sqlite3 *DB){
   std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
   insertCat(DB, desiredName);
 }
-void removeItem(sqlite3 *DB){
+void removeItem(sqlite3 *DB) {
   std::string desiredName;
   std::cout << "\n\nPlease provide the name of the item: ";
   std::cin >> desiredName;
   std::cin.clear();
   std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
   int deleteVerifier = deleteItem(DB, desiredName);
-   
 }
-void searchTable(sqlite3 *DB){
+void searchTable(sqlite3 *DB) {
   std::string desiredName;
   std::cout << "\n\nPlease provide the name of the item to search: ";
   std::cin >> desiredName;
@@ -122,4 +124,3 @@ void searchTable(sqlite3 *DB){
   std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
   searchItem(DB, desiredName);
 }
-
